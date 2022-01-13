3fa899abb8757cdbc4c4577adf6f4a6db0301687
Svitlana Pushchalo <svitlana8@gmail.com>
2022-01-13 19:10:11 +0200

    conftest_commandline

--- session3/lesson6/1ex_conftest_cmd/test_parser.py
+++ session3/lesson6/1ex_conftest_cmd/test_parser.py
@@ -6,14 +6,18 @@ def test_guest_should_see_login_link(browser):
     browser.find_element_by_css_selector("#login_link")
 
 
-# Если мы теперь запустим тесты БЕЗ параметра, то НЕ получим ошибку:
+
+# Если вы запустите тесты без параметра, то получите ОШИБКУ:
 # $ pytest -s -v test_parser.py
-# и наши тесты запустятся в браузере Chrome (который задан по умолчанию в conftest.py)
+# ERROR test_parser.py::test_guest_should_see_login_link - _pytest.config.exceptions.UsageError: --browser_name should be chrome or firefox
 #
-# если все же при запуске тестов укажем параметр --browser_name=chrome,
-# pytest -s -v --browser_name=chrome test_parser.py
-# то наши тесты также запустятся в браузере Chrome.
 #
-# теперь запустим тесты на firefox (--browser_name=firefox):
+# Запустим тесты на Firefox.
+# Для етого при запуске тестов нужно указать параметр --browser_name=firefox,
 # pytest -s -v --browser_name=firefox test_parser.py
-# мы должны увидеть, как тесты запустятся в браузере Firefox
+# и мы должны увидеть, как тесты запустятся в браузере Firefox
+#
+# А теперь запустим тесты на Chrome.
+# Для етого при запуске тестов нужно указать параметр --browser_name=chrome:
+# pytest -s -v --browser_name=chrome test_parser.py
+# мы должны увидеть, как тесты запустятся в браузере Chrome.
